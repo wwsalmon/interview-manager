e04c08c08d7fd9b733c31f029a2a6ae133a6a7de
Samson Zhang <wwsamson@yahoo.com>
2023-06-05 00:20:58 -0700

    wip folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,27 @@
+import { readDir, createDir, BaseDirectory, exists } from "@tauri-apps/api/fs";
+import { message } from "@tauri-apps/api/dialog";
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [folders, setFolders] = useState<any[] | null>(null);
+
+  useEffect(() => {
+    async function onLoad() {
+      const hasAppFolder = await exists("szh-interviews", { dir: BaseDirectory.Desktop });
+
+      if (!hasAppFolder) {
+        await createDir("szh-interviews", { dir: BaseDirectory.Desktop });
+        setFolders([]);
+      } else {
+        const folders = await readDir("szh-interviews", { dir: BaseDirectory.Desktop, recursive: true });
+        setFolders(folders);
+      }
+    }
+
+    onLoad();
+  }, []);
+
+  console.log(folders);
 
   return (
     <div>
