106bcce48da4dd270ff55110b3fe352838f846b4
Samson Zhang <wwsamson@yahoo.com>
2023-06-10 02:18:29 -0700

    add menubar open folder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,8 +10,44 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let firstmenu = Submenu::new("First", Menu::new()
+        .add_native_item(MenuItem::Quit)
+    );
+
+    let filemenu = Submenu::new("File", Menu::new()
+        .add_item(CustomMenuItem::new("open", "Open Folder").accelerator("cmdOrControl+O"))
+        .add_item(CustomMenuItem::new("new", "New File").accelerator("cmdOrControl+N"))
+    );
+
+    let editmenu = Submenu::new("Edit", Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_native_item(MenuItem::Cut)
+        .add_native_item(MenuItem::Paste)
+    );
+
+    let windowmenu = Submenu::new("Window", Menu::new()
+        .add_native_item(MenuItem::CloseWindow)
+        .add_item(CustomMenuItem::new("new_window", "New Window").accelerator("cmdOrControl+shift+N"))
+    );
+
+    let menu = Menu::new()
+        .add_submenu(firstmenu)
+        .add_submenu(filemenu)
+        .add_submenu(editmenu)
+        .add_submenu(windowmenu);
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id() {
+            "open" => {
+                let _ = event.window().emit("menu-event", "open-event");
+            }
+            "new" => {
+                let _ = event.window().emit("menu-event", "new-event");
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,92 @@
-import { readDir, createDir, BaseDirectory, exists } from "@tauri-apps/api/fs";
-import { message } from "@tauri-apps/api/dialog";
+import { listen } from "@tauri-apps/api/event";
+import { open } from "@tauri-apps/api/dialog";
 import { useEffect, useState } from "react";
+import { readDir, BaseDirectory, FileEntry } from "@tauri-apps/api/fs";
+import classNames from "classnames";
 
 export default function App() {
-  const [folders, setFolders] = useState<any[] | null>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isNew, setIsNew] = useState<boolean>(false);
+  const [dir, setDir] = useState<string | null>();
+  const [contents, setContents] = useState<FileEntry[]>([]);
+  const [selected, setSelected] = useState<string | null>(null);
 
   useEffect(() => {
-    async function onLoad() {
-      const hasAppFolder = await exists("szh-interviews", { dir: BaseDirectory.Desktop });
-
-      if (!hasAppFolder) {
-        await createDir("szh-interviews", { dir: BaseDirectory.Desktop });
-        setFolders([]);
-      } else {
-        const folders = await readDir("szh-interviews", { dir: BaseDirectory.Desktop, recursive: true });
-        setFolders(folders);
+    listen("menu-event", e => {
+      try {
+        if (e.payload === "open-event") {
+          setIsOpen(true);
+        } else if (e.payload === "new-event") {
+          setIsNew(true);
+        }
+      } catch (e) {
+        console.log(e);
       }
-    }
-
-    onLoad();
+    });
   }, []);
 
-  console.log(folders);
+  useEffect(() => {
+    if (isOpen) {
+      OnOpen();
+      setIsOpen(false);
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (isNew) {
+      OnNew();
+      setIsNew(false);
+    }
+  }, [isNew]);
+
+  async function OnOpen() {
+    let filepath = await open({directory: true});
+    setDir(filepath as string);
+  }
+
+  async function OnNew() {
+    
+  }
+
+  useEffect(() => {
+    if (dir) {
+      AfterOpen();
+    }
+  }, [dir]);
+
+  async function AfterOpen() {
+    if (dir) {
+      let files = await readDir(dir, { dir: BaseDirectory.Home, recursive: false });
+      files = files.filter(d => d.name !== ".DS_Store");
+      console.log(files);
+      setContents(files);
+    }
+  }
+
+  function getProjectName() {
+    if (!dir) return "";
+    const dirSplit = dir.split("/");
+    return dirSplit[dirSplit.length - 1];
+  }
 
   return (
     <div>
-      <p className="p-4 text-center bg-gray-100">hello world</p>
+      {dir ? (
+        <div className="flex h-full min-h-screen">
+          <div className="w-64 bg-gray-100">
+            <p className="p-2 break-all border-b text-sm opacity-50">Project: {getProjectName()}</p>
+            {contents.map((d) => (
+              <button
+                className={classNames("p-4 block w-full text-left", selected === d.name ? "bg-white disabled" : "hover:bg-gray-200")}
+                key={d.name}
+                onClick={() => setSelected(d.name as string)}
+              >{d.name}</button>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <p className="p-4 text-center">No folder open</p>
+      )}
     </div>
   );
 }
