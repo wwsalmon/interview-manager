0a1dc7c7033d409ce01e8482dfb3f4ec8d523bdf
Samson Zhang <wwsamson@yahoo.com>
2023-06-10 02:37:32 -0700

    new file modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { open } from "@tauri-apps/api/dialog";
 import { useEffect, useState } from "react";
 import { readDir, BaseDirectory, FileEntry } from "@tauri-apps/api/fs";
 import classNames from "classnames";
+import Modal from "react-modal";
 
 export default function App() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isNew, setIsNew] = useState<boolean>(false);
+  const [isNewModal, setIsNewModal] = useState<boolean>(false);
   const [dir, setDir] = useState<string | null>();
   const [contents, setContents] = useState<FileEntry[]>([]);
   const [selected, setSelected] = useState<string | null>(null);
@@ -45,7 +47,7 @@ export default function App() {
   }
 
   async function OnNew() {
-    
+    setIsNewModal(true);
   }
 
   useEffect(() => {
@@ -83,6 +85,29 @@ export default function App() {
               >{d.name}</button>
             ))}
           </div>
+          <Modal
+            isOpen={isNewModal}
+            onRequestClose={() => setIsNewModal(false)}
+            style={{
+              overlay: {
+                backgroundColor: "rgba(0,0,0,0.5)",
+              },
+              content: {
+                maxWidth: "400px",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%, -50%)",
+                height: "320px",
+              }
+            }}
+          >
+            <p className="text-center mb-6 font-bold">New file</p>
+            <p className="uppercase mb-2 text-sm tracking-wide font-bold">Name</p>
+            <input type="text" className="border w-full p-2 mb-6" placeholder="Tim interview"/>
+            <p className="uppercase mb-2 text-sm tracking-wide font-bold">Date</p>
+            <input type="date" className="border w-full p-2 mb-6"/>
+            <button className="w-full bg-gray-800 p-2 text-white mt-auto">Create</button>
+          </Modal>
         </div>
       ) : (
         <p className="p-4 text-center">No folder open</p>
