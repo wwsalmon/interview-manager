679df207b6d0dca09cae20e665f249b3aba5c889
Samson Zhang <wwsamson@yahoo.com>
2023-06-10 03:22:48 -0700

    add wip file saving and reading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { listen } from "@tauri-apps/api/event";
 import { open } from "@tauri-apps/api/dialog";
 import { useEffect, useState } from "react";
-import { readDir, BaseDirectory, FileEntry } from "@tauri-apps/api/fs";
+import { readDir, BaseDirectory, FileEntry, writeTextFile, readTextFile } from "@tauri-apps/api/fs";
 import classNames from "classnames";
 import Modal from "react-modal";
+import fm from "front-matter";
 
 export default function App() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -12,6 +13,16 @@ export default function App() {
   const [dir, setDir] = useState<string | null>();
   const [contents, setContents] = useState<FileEntry[]>([]);
   const [selected, setSelected] = useState<string | null>(null);
+  
+  const [newName, setNewName] = useState<string>("");
+  const [newDate, setNewDate] = useState<string>("");
+
+  const [newUrl, setNewUrl] = useState<string>("");
+
+  const [name, setName] = useState<string>("");
+  const [date, setDate] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+  const [notes, setNotes] = useState<string>("");
 
   useEffect(() => {
     listen("menu-event", e => {
@@ -29,38 +40,37 @@ export default function App() {
 
   useEffect(() => {
     if (isOpen) {
-      OnOpen();
+      onOpen();
       setIsOpen(false);
     }
   }, [isOpen]);
 
   useEffect(() => {
     if (isNew) {
-      OnNew();
+      onNew();
       setIsNew(false);
     }
   }, [isNew]);
 
-  async function OnOpen() {
+  async function onOpen() {
     let filepath = await open({directory: true});
     setDir(filepath as string);
   }
 
-  async function OnNew() {
+  async function onNew() {
     setIsNewModal(true);
   }
 
   useEffect(() => {
     if (dir) {
-      AfterOpen();
+      afterOpen();
     }
   }, [dir]);
 
-  async function AfterOpen() {
+  async function afterOpen() {
     if (dir) {
       let files = await readDir(dir, { dir: BaseDirectory.Home, recursive: false });
-      files = files.filter(d => d.name !== ".DS_Store");
-      console.log(files);
+      files = files.filter(d => d.name?.substring(d.name.length - 10) === ".interview");
       setContents(files);
     }
   }
@@ -71,11 +81,50 @@ export default function App() {
     return dirSplit[dirSplit.length - 1];
   }
 
+  async function onCreate() {
+    const fileContent = `---
+name: "${newName}"
+date: "${newDate}"
+notes: ""
+---`;
+
+    const fileName = encodeURIComponent(newName) + ".interview";
+
+    await writeTextFile(dir + "/" + fileName, fileContent, {dir: BaseDirectory.Home});
+
+    await afterOpen();
+
+    setSelected(fileName);
+    setNewName("");
+    setNewDate("");
+    setIsNewModal(false);
+  }
+
+  useEffect(() => {
+    if (dir && selected) {
+      onSelect();
+    }
+  }, [selected]);
+
+  async function onSelect() {
+    const content = await readTextFile(dir + "/" + selected, {dir: BaseDirectory.AppConfig});
+
+    const parsed = fm(content);
+    const attributes = parsed.attributes as {[key: string]: string};
+
+    if (!(attributes.name && attributes.date && "notes" in attributes)) return;
+
+    setName(attributes.name);
+    setDate(attributes.date);
+    setNotes(attributes.notes);
+    setBody(parsed.body);
+  }
+
   return (
     <div>
       {dir ? (
         <div className="flex h-full min-h-screen">
-          <div className="w-64 bg-gray-100">
+          <div className="w-64 bg-gray-100 flex-shrink-0">
             <p className="p-2 break-all border-b text-sm opacity-50">Project: {getProjectName()}</p>
             {contents.map((d) => (
               <button
@@ -103,11 +152,18 @@ export default function App() {
           >
             <p className="text-center mb-6 font-bold">New file</p>
             <p className="uppercase mb-2 text-sm tracking-wide font-bold">Name</p>
-            <input type="text" className="border w-full p-2 mb-6" placeholder="Tim interview"/>
+            <input type="text" className="border w-full p-2 mb-6" placeholder="Tim interview" value={newName} onChange={e => setNewName(e.target.value)}/>
             <p className="uppercase mb-2 text-sm tracking-wide font-bold">Date</p>
-            <input type="date" className="border w-full p-2 mb-6"/>
-            <button className="w-full bg-gray-800 p-2 text-white mt-auto">Create</button>
+            <input type="date" className="border w-full p-2 mb-6" value={newDate} onChange={e => setNewDate(e.target.value)}/>
+            <button
+              className="w-full bg-gray-800 p-2 text-white mt-auto disabled:opacity-50"
+              disabled={!(newName && newDate)}
+              onClick={onCreate}
+            >Create</button>
           </Modal>
+          <div className="w-full">
+            <p>date: {date}, name: {name}</p>
+          </div>
         </div>
       ) : (
         <p className="p-4 text-center">No folder open</p>
