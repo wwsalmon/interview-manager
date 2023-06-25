10c3c4c1cfa3d74be48b160cba01e91716401df9
Samson Zhang <wwsamson@yahoo.com>
2023-06-25 03:42:28 -0700

    close #13

--- src/components/Website.tsx
+++ src/components/Website.tsx
@@ -13,6 +13,7 @@ export default function Website({dir, selected, afterDelete, afterSave}: {dir: s
     const [contents, setContents] = useState<{name: string, date: string, body: string, url: string, pub: string}>({name: "", date: "", body: "", url: "", pub: ""});
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [isSaving, setIsSaving] = useState<boolean>(false);
+    const [showWebsite, setShowWebsite] = useState<boolean>(false);
 
     const hasUnsaved = (contents.body !== body) || (contents.date !== date) || (contents.pub !== pub) || (contents.name !== name);
 
@@ -98,7 +99,21 @@ export default function Website({dir, selected, afterDelete, afterSave}: {dir: s
             onDelete={onDelete}
         >
             <div className="w-1/2 flex-shrink-0 flex-grow-0 border-r h-full overflow-hidden">
-                <iframe src={url} className="w-[133%] h-[133%] transform scale-75 origin-top-left"></iframe>
+                {showWebsite ? (
+                    <iframe src={url} className="w-[133%] h-[133%] transform scale-75 origin-top-left"></iframe>
+                ) : (
+                    <div className="p-8">
+                        <AreaLabel>{contents.pub}</AreaLabel>
+                        <p className="-mt-4 text-xl mb-4 opacity-50">{contents.name}</p>
+                        <p className="mb-12 text-sm opacity-50">{url}</p>
+                        <AreaLabel>Show website</AreaLabel>
+                        <div className="flex items-center mb-8">
+                            <a href={url} className="text-sm text-white px-4 py-1 bg-gray-800 block" target="_blank">Open in new window</a>
+                            <button onClick={() => setShowWebsite(true)} className="px-4 py-1 text-sm text-white bg-gray-800 ml-4">Open in app</button>
+                        </div>
+                        <p className="opacity-50 text-xs">Opening the website in app may fail as some websites don't allow being put in iframe</p>
+                    </div>
+                )}
             </div>
             <HalfContainer>
                 <AreaLabel>Notes</AreaLabel>
